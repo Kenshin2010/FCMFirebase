4bbe587b44c2f7882dc4a55866f250969b11332a
Manroid38 <phamdoanmanu38@gmail.com>
2017-02-08 09:32:33 +0700

    Changed

---
+++ app/src/main/java/com/manroid/fcmfirebase/GitContinue.java
@@ -0,0 +1,8 @@
+package com.manroid.fcmfirebase;
+
+/**
+ * Created by Manroid on 2/8/2017.
+ */
+
+public class GitContinue {
+}
--- app/src/main/java/com/manroid/fcmfirebase/MainActivity.java
+++ app/src/main/java/com/manroid/fcmfirebase/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Github",Toast.LENGTH_LONG).show();
         btn_sub = (Button)findViewById(R.id.button);
         btn_sub.setOnClickListener(new View.OnClickListener() {
             @Override
