9ab59aec944867dfb4066616275c12fded33c119
Manroid38 <phamdoanmanu38@gmail.com>
2017-02-08 09:20:07 +0700

    updating

---
+++ app/src/main/java/com/manroid/fcmfirebase/MainActivity.java
@@ -0,0 +1,25 @@
+package com.manroid.fcmfirebase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_sub;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_sub = (Button)findViewById(R.id.button);
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseMessaging.getInstance().subscribeToTopic("Technology");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/manroid/fcmfirebase/MyFirebaseFCM.java
@@ -0,0 +1,56 @@
+package com.manroid.fcmfirebase;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Manroid on 2/4/2017.
+ */
+
+public class MyFirebaseFCM extends FirebaseMessagingService {
+    String TAG = "MyFirebase";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if (remoteMessage.getNotification() != null){
+            Log.e(TAG,remoteMessage.getNotification().getBody());
+           // Log.e(TAG,remoteMessage.getNotification().getTitle());
+            sendMessage(remoteMessage.getNotification().getBody());
+
+
+        }
+
+        if (remoteMessage.getData().size() > 0){
+            Log.e(TAG,remoteMessage.getData() + "");
+            Log.e(TAG,remoteMessage.getData().get("k1"));
+        }
+    }
+
+    private void sendMessage(String messageBody){
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("FCM Manager")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
+
+    }
+}
